2982341db6597f50de5d63e6bf0e01b48fe1a62e
Mikhail Dyakonov <dyakonov.mikhail@gmail.com>
2019-08-14 16:10:03 +0400

    Adding charts

---
+++ modules/web/src/com/company/stockman/web/screens/product/PriceStats.java
@@ -0,0 +1,18 @@
+package com.company.stockman.web.screens.product;
+
+import com.haulmont.cuba.gui.screen.LoadDataBeforeShow;
+import com.haulmont.cuba.gui.screen.Screen;
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
+
+
+/**
+ *
+ * @author dyakonov
+ * @version $Id$
+ */
+@UiController("stockman_PriceStats")
+@UiDescriptor("price-stats.xml")
+@LoadDataBeforeShow
+public class PriceStats extends Screen {
+}
